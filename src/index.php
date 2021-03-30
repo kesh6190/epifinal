5b7e40f5fdc012c3e8b530a01c084f5c27ad0c54
kesh6190 <skchundunsing@gmail.com>
2021-03-30 11:28:56 +0400

    git added all necessary files to run core app with docker

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+	$x=10.0;
+	$y=-4;
+	echo "Simple calculation stuff...</br>";
+	echo "</br>x=".$x;
+	echo "</br>y=".$y;
+	echo "</br>---------------------------";
+	echo "</br>x+y=".($x+$y);
+    	echo "</br>x-y=".($x-$y);
+    	echo "</br>x*y=".($x*$y);
+    	echo "</br>x**y=".($x**$y);
+    	echo "</br>x/y=".($x/$y);
+    	echo "</br>x%y=".($x%$y);
+    	echo "This is your final practice test page😐️";
+?>
+
